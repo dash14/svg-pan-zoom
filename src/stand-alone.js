edfeed21319e02993cb4fa6fc0357eff142c419c
dash14 <dash14.ack@gmail.com>
2021-07-19 00:01:47 +0900

    Commit the result of running the build process

--- src/stand-alone.js
+++ src/stand-alone.js
@@ -2,10 +2,10 @@ var svgPanZoom = require("./svg-pan-zoom.js");
 var Utils = require("./utilities");
 
 // UMD module definition
-(function(_this) {
+(function (_this) {
   // AMD
   if (typeof define === "function" && define.amd) {
-    define("svg-pan-zoom", function() {
+    define("svg-pan-zoom", function () {
       return svgPanZoom;
     });
     // CMD
