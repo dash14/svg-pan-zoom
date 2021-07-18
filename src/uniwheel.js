af83181409ec79513085ced1499c50f05b7f777c
dash14 <dash14.ack@gmail.com>
2021-07-18 23:23:14 +0900

    Don't access `window` when exporting the module

--- src/uniwheel.js
+++ src/uniwheel.js
@@ -2,15 +2,18 @@
 // A unified cross browser mouse wheel event handler
 // https://github.com/teemualap/uniwheel
 
-module.exports = (function(){
-
+module.exports = (function() {
   //Full details: https://developer.mozilla.org/en-US/docs/Web/Reference/Events/wheel
 
-  var prefix = "", _addEventListener, _removeEventListener, support, fns = [];
-  var passiveOption = {passive: true};
+  var prefix = "",
+    _addEventListener,
+    _removeEventListener,
+    support,
+    fns = [];
+  var passiveOption = { passive: true };
 
   // detect event model
-  if ( window.addEventListener ) {
+  if (globalThis.addEventListener) {
     _addEventListener = "addEventListener";
     _removeEventListener = "removeEventListener";
   } else {
@@ -19,17 +22,17 @@ module.exports = (function(){
     prefix = "on";
   }
 
-  // detect available wheel event
-  support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-            document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-            "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
-
-
-  function createCallback(element,callback) {
+  function createCallback(element, callback) {
+    // detect available wheel event
+    support =
+      "onwheel" in document.createElement("div")
+        ? "wheel" // Modern browsers support "wheel"
+        : document.onmousewheel !== undefined
+        ? "mousewheel" // Webkit and IE support at least "mousewheel"
+        : "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
 
     var fn = function(originalEvent) {
-
-      !originalEvent && ( originalEvent = window.event );
+      !originalEvent && (originalEvent = window.event);
 
       // create a normalized event object
       var event = {
@@ -41,29 +44,29 @@ module.exports = (function(){
         deltaX: 0,
         delatZ: 0,
         preventDefault: function() {
-          originalEvent.preventDefault ?
-            originalEvent.preventDefault() :
-            originalEvent.returnValue = false;
+          originalEvent.preventDefault
+            ? originalEvent.preventDefault()
+            : (originalEvent.returnValue = false);
         }
       };
 
       // calculate deltaY (and deltaX) according to the event
-      if ( support == "mousewheel" ) {
-        event.deltaY = - 1/40 * originalEvent.wheelDelta;
+      if (support == "mousewheel") {
+        event.deltaY = (-1 / 40) * originalEvent.wheelDelta;
         // Webkit also support wheelDeltaX
-        originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
+        originalEvent.wheelDeltaX &&
+          (event.deltaX = (-1 / 40) * originalEvent.wheelDeltaX);
       } else {
         event.deltaY = originalEvent.detail;
       }
 
       // it's time to fire the callback
-      return callback( event );
-
+      return callback(event);
     };
 
     fns.push({
       element: element,
-      fn: fn,
+      fn: fn
     });
 
     return fn;
@@ -75,18 +78,18 @@ module.exports = (function(){
         return fns[i].fn;
       }
     }
-    return function(){};
+    return function() {};
   }
 
   function removeCallback(element) {
     for (var i = 0; i < fns.length; i++) {
       if (fns[i].element === element) {
-        return fns.splice(i,1);
+        return fns.splice(i, 1);
       }
     }
   }
 
-  function _addWheelListener(elem, eventName, callback, isPassiveListener ) {
+  function _addWheelListener(elem, eventName, callback, isPassiveListener) {
     var cb;
 
     if (support === "wheel") {
@@ -95,11 +98,14 @@ module.exports = (function(){
       cb = createCallback(elem, callback);
     }
 
-    elem[_addEventListener](prefix + eventName, cb, isPassiveListener ? passiveOption : false);
+    elem[_addEventListener](
+      prefix + eventName,
+      cb,
+      isPassiveListener ? passiveOption : false
+    );
   }
 
-  function _removeWheelListener(elem, eventName, callback, isPassiveListener ) {
-
+  function _removeWheelListener(elem, eventName, callback, isPassiveListener) {
     var cb;
 
     if (support === "wheel") {
@@ -108,26 +114,40 @@ module.exports = (function(){
       cb = getCallback(elem);
     }
 
-    elem[_removeEventListener](prefix + eventName, cb, isPassiveListener ? passiveOption : false);
+    elem[_removeEventListener](
+      prefix + eventName,
+      cb,
+      isPassiveListener ? passiveOption : false
+    );
 
     removeCallback(elem);
   }
 
-  function addWheelListener( elem, callback, isPassiveListener ) {
-    _addWheelListener(elem, support, callback, isPassiveListener );
+  function addWheelListener(elem, callback, isPassiveListener) {
+    _addWheelListener(elem, support, callback, isPassiveListener);
 
     // handle MozMousePixelScroll in older Firefox
-    if( support == "DOMMouseScroll" ) {
-      _addWheelListener(elem, "MozMousePixelScroll", callback, isPassiveListener );
+    if (support == "DOMMouseScroll") {
+      _addWheelListener(
+        elem,
+        "MozMousePixelScroll",
+        callback,
+        isPassiveListener
+      );
     }
   }
 
-  function removeWheelListener(elem, callback, isPassiveListener){
+  function removeWheelListener(elem, callback, isPassiveListener) {
     _removeWheelListener(elem, support, callback, isPassiveListener);
 
     // handle MozMousePixelScroll in older Firefox
-    if( support == "DOMMouseScroll" ) {
-      _removeWheelListener(elem, "MozMousePixelScroll", callback, isPassiveListener);
+    if (support == "DOMMouseScroll") {
+      _removeWheelListener(
+        elem,
+        "MozMousePixelScroll",
+        callback,
+        isPassiveListener
+      );
     }
   }
 
@@ -135,5 +155,4 @@ module.exports = (function(){
     on: addWheelListener,
     off: removeWheelListener
   };
-
 })();
